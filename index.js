722bf9f884e2a2db3f0dea2897fb2dc4963dc8f0
siddhantv45 <75495154+siddhantv45@users.noreply.github.com>
2021-10-10 03:20:30 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const routes = require('./routes/product_route')
+const usermodel = require('./models/product')
+const ejs = require('ejs');
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+mongoose.Promise = global.Promise;
+const CONNECTION_URL = 'mongodb+srv://Esha:0wtR2WggVzAiE9un@cluster0.ygb7z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 8081;
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
+
+const app = express();
+app.use(morgan('combined '))
+app.use(bodyParser.json())
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.set('view engine', 'ejs');
+
+//app.use('/', routes);
+routes(app);
+console.log(mongoose.connection.readyState)
+
+
+
+
+
+
